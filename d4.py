00d441395823e9423cf4eed35ae872ac20b97177
Benjamin Jones <benjaminfjones@gmail.com>
2018-12-09 15:38:54 -0800

    day 4, problems 1 & 2

--- d4.py
+++ d4.py
@@ -1,11 +1,10 @@
 import datetime
-import pprint
 import re
-from typing import List, Dict, Tuple, Union
+from typing import Any, List, Dict, Tuple, Union
+
 
 # test data
-test_data_s: str = """\
-[1518-11-01 00:00] Guard #10 begins shift
+test_data_s: str = """[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
 [1518-11-01 00:30] falls asleep
@@ -35,24 +34,24 @@ Day = datetime.datetime        # only  ymd
 GuardState = int
 GuardID = int
 # action strings -> either int or bool (depending on key)
-ParsedAction = Dict[str, Union[GuardID, str]]
+ParsedAction = Dict[str, Union[GuardID, bool]]
 # date/time -> action that happened starting then
 ParsedEvent = Tuple[EventTime, ParsedAction]
 
 
 def parse_line(inp: str) -> ParsedEvent:
     """ Returns (year,month,day,hour,min,action) """
-    r = re.match(r'\[(\d+)\-(\d+)\-(\d+) (\d+):(\d+)\] (.+)', inp)
-    t: List[str] = list(r.groups())
-    ymd: List[int] = map(lambda x: int(x), t[:5])
-    dt = datetime.datetime(*ymd)
 
-    i = re.match(r'Guard #(\d+) begins shift', t[5])
+    datestr, _, rest = inp.partition(']')
+    dt = datetime.datetime.strptime(datestr, '[%Y-%m-%d %H:%M')
+    act = rest.lstrip()
+
+    i = re.match(r'Guard #(\d+) begins shift', act)
     if i:
         p = {'Guard': int(i.group(1))}
-    elif re.match(r'falls asleep', t[5]):
+    elif re.match(r'falls asleep', act):
         p = {'falls asleep': True}
-    elif re.match(r'wakes up', t[5]):
+    elif re.match(r'wakes up', act):
         p = {'wakes up': True}
     else:
         raise Exception('foobar')
@@ -60,34 +59,147 @@ def parse_line(inp: str) -> ParsedEvent:
     return (dt, p)
 
 
-def organize(xs: List[ParsedEvent]) -> \
-            Dict[datetime.datetime, List[ParsedEvent]]:
-    res = {}
+def organize_days(xs: List[ParsedEvent]) -> \
+                  Dict[Day, List[ParsedEvent]]:
+    """
+    Takes a list of events and organizes them by day, fixing the entries
+    which occur in hour 23 of a previous day to appear that they took place at
+    00:00 on the next.
+    """
+    res: Dict[Day, List[ParsedEvent]] = {}
     delta_day = datetime.timedelta(1)
     for x in xs:
+
         # fix hour 23 guard events to start at next day 00:00
         dt = x[0]
         if dt.hour == 23:
             dt = dt + delta_day
             dt = dt.replace(hour=0, minute=0)
+
+        # keep only the day
+        dt = datetime.datetime(dt.year, dt.month, dt.day)
+
         if dt not in res:
-            res[dt] = x
+            res[dt] = [x]
         else:
             res[dt].append(x)
+
     return res
 
 
-def process_events(xs: Dict[EventTime, List[ParsedEvent]]) -> \
+def process_events(xs: List[ParsedEvent]) -> \
                    Tuple[GuardID, Dict[int, GuardState]]:
-    gid: int = -1
-    last_time: int = 0
-    for (dt, ys) in xs.items():
-        for a, v in ys:
-            if a == 'Guard':
-                gid = v
-            elif a == 'falls asleep':
+    """
+    Interpret a list of events from each day, returning the guard ID of the
+    guard on duty as well as a map from minutes to conscious state.
+    """
+    if len(xs) == 0:
+        raise Exception('No events to process!')
+    xs_sorted = sorted(xs, key=lambda x: x[0])
+
+    gtuple = xs_sorted[0]
+    gtime, gact = gtuple
+
+    ##########################
+    # Event processing state
+    guard_log: Dict[int, GuardState] = {}
+    last_time: int = -1
+    last_state: GuardState = -1
+    ##########################
+
+    if 'Guard' in gact:
+        gid: int = gact['Guard']
+        last_time = gtime.minute
+        last_state = 1  # awake!
+        for m in range(last_time):
+            guard_log[m] = -1  # asleep or absent
+    else:
+        raise Exception('No guard on duty!')
+
+    # process rest of events
+    for evt_time, evt in xs_sorted[1:]:
+        evt_min = evt_time.minute
+
+        if 'falls asleep' in evt:
+            if last_state != 1:
+                raise Exception('Guard fell asleep while absent or asleep!')
+            for m in range(last_time, evt_min):
+                guard_log[m] = 1  # awake
+            last_time = evt_min
+            last_state = 0  # asleep
+
+        elif 'wakes up' in evt:
+            if last_state != 0:
+                raise Exception('Guard woke up while awake!')
+            for m in range(last_time, evt_min):
+                guard_log[m] = 0  # asleep
+            last_time = evt_min
+            last_state = 1  # awake
+
+        else:
+            raise Exception('Invalid action while guard on duty:', evt)
+
+    # fill the remainder of the log
+    for m in range(last_time, 60):
+        guard_log[m] = last_state
+
+    return (gid, guard_log)
+
+
+def find_max_value(d: Dict[Any, int]) -> Tuple[Any, int]:
+    ditems = list(d.items())
+    return max(*ditems, key=lambda x: x[1])
 
 
 def main():
-    xs = map(parse_line, test_data)
-    pprint.pprint(organize(xs))
+    with open('input4') as f:
+        inp: str = f.readlines()
+    xs = map(parse_line, inp)
+    ds = organize_days(xs)
+
+    gl = {}
+    for day, ls in ds.items():
+        gid, log = process_events(ls)
+        # accumulate guard logs
+        if gid not in gl:
+            gl[gid] = [log]
+        else:
+            gl[gid].append(log)
+
+    # count sleeping minutes
+    sleepy_mins: Dict[GuardID, int] = {}
+    sleepiest_min: Dict[GuardID, int] = {}
+    for gid, lgs in gl.items():
+        asleep_histo: Dict[int, int] = {}
+        for l in lgs:
+            for m, state in l.items():
+                if state == 0:
+                    if m in asleep_histo:
+                        asleep_histo[m] = asleep_histo[m] + 1
+                    else:
+                        asleep_histo[m] = 1
+
+        # sum minutes asleep
+        s: int = 0
+        for _, v in asleep_histo.items():
+            s = s + v
+        sleepy_mins[gid] = s
+
+        # find minute that  guard is most often asleep
+        if asleep_histo:  # a guard might never fall asleep!
+            sleepiest_min[gid] = find_max_value(asleep_histo)
+
+    # Solution for part 1
+    gid, amt = find_max_value(sleepy_mins)
+    print('Guard', gid, 'asleep longest for', amt, 'mins')
+    print('Guard', gid, 'asleep at min', sleepiest_min[gid][0], 'most often')
+
+    # Solution for part 2
+    sgm = list(sleepiest_min.items())
+    sgm_max = max(*sgm, key=lambda x: x[1][1])
+    print('Guard', sgm_max[0], 'asleep at min', sgm_max[1][0], 'most at',
+          sgm_max[1][1], 'minutes')
+
+
+if __name__ == '__main__':
+    main()
